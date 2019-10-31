e29efbcb1478ffd1ad32b03d4cf50f9baa476a4b
Christian Woehrle <christian.woehrle@dwpbank.de>
2019-10-31 20:57:41 +0100

    sleep to let the istio sidecar becme ready

--- postgres/cleanup.sh
+++ postgres/cleanup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+echo "URL for postgres Master: ${URL_MASTER}"
+
+
+echo "sleep for 10 seconds in case an istio sidecar has to come up"
+sleep 10
+
 
 psql -h ${URL_MASTER} -U ${USERNAME} -c "drop table posts" 
 
