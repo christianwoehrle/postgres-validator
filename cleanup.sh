4fbbada8a9118f769a317fe5889a893c99aac47b
Christian Woehrle <christian.woehrle@dwpbank.de>
2019-10-31 20:00:03 +0100

    cleanup database

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,20 @@
 #/bin/sh
 
 
-psql -h ${URL} -U ${USERNAME} -c "drop database test;"
+psql -h ${URL_MASTER} -U ${USERNAME} -c "drop table posts" 
+
+if [ $? != 0 ]
+then
+        echo "Error dropping table on the master"
+fi
+
+psql -h ${URL_MASTER} -U ${USERNAME} -c "drop database test;"
 if [ $? != 0 ]  
 then
 	echo "Error dropping database test"
 	exit 1
 fi
 
-
+echo "success"
 exit 0
 
